dc7f6fc9d38415da8f2b5a9c233a7fa9b1af9a86
Mike Lock <michael.lockwitz@consensys.net>
2019-05-08 15:44:07 -0600

    Updates

---
+++ precache-manifest.a343ade4ca602f20832e4347f031bd00.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccd5f05ee238f120a91f06a4f21e316",
+    "url": "/rimble-app-demo/index.html"
+  },
+  {
+    "revision": "210f6d9363e16d1f1ff6",
+    "url": "/rimble-app-demo/static/js/2.7c2aad84.chunk.js"
+  },
+  {
+    "revision": "fd557d232f9df109eeda",
+    "url": "/rimble-app-demo/static/js/main.ed1a15bc.chunk.js"
+  },
+  {
+    "revision": "103de803e59fd69d869e",
+    "url": "/rimble-app-demo/static/js/runtime~main.979c2ad8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rimble-app-demo/precache-manifest.36cf9681af23fee5d7890954ef1b7a0c.js"
+  "/rimble-app-demo/precache-manifest.a343ade4ca602f20832e4347f031bd00.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rimble-app-demo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rimble-app-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
